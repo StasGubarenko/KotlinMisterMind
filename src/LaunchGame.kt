808d775826bd352cb4fbb5fed8f230a87f110950
Stas <120662529+StasGubarenko@users.noreply.github.com>
2023-05-20 12:32:57 +0600

    Update LaunchGame.kt

--- src/LaunchGame.kt
+++ src/LaunchGame.kt
@@ -4,7 +4,7 @@ fun main() {
 
     val size: Int? = inputCoder?.length
 
-    val inputCodebreaker: String? = inputCoderBreaker(size)
+    val inputCodebreaker: String? = inputCodebreaker(size)
 
     compareTwoLetters(inputCoder, inputCodebreaker, size)
 
@@ -16,134 +16,134 @@ fun inputCoder(): String? {
     return readLine()
 }
 
-fun inputCoderBreaker(sizeOfInputtedLetters: Int?): String? {
+fun inputCodebreaker(sizeOfInputtedLetters: Int?): String? {
 
     println("\nCodebreaker, input any sequence of letters consisting of $sizeOfInputtedLetters")
 
     //Переменная для хранения символов, которые ввел codebreaker
-    var inputDecoder = readLine()
+    var inputCodebreaker = readLine()
 
     //длина символов, которые были введены codebreaker
-    var sizeOfInputtedDecoderLetters = inputDecoder?.length
+    var sizeOfInputtedCodebreakerLetters = inputCodebreaker?.length
 
     var switcher = true
 
     //Проверяем количество символов
     while (switcher) {
-        if (sizeOfInputtedDecoderLetters == sizeOfInputtedLetters) {
+        if (sizeOfInputtedCodebreakerLetters == sizeOfInputtedLetters) {
             switcher = false
         } else {
             println("Sorry, you entered the wrong number of characters. You must enter $sizeOfInputtedLetters characters")
-            inputDecoder = readLine()
-            sizeOfInputtedDecoderLetters = inputDecoder?.length
+            inputCodebreaker = readLine()
+            sizeOfInputtedCodebreakerLetters = inputCodebreaker?.length
         }
     }
-    return inputDecoder
+    return inputCodebreaker
 }
 
-fun compareTwoLetters(inputCoder: String?, inputCoderBreaker: String?, size: Int?) {
+fun compareTwoLetters(inputCoder: String?, inputCoderbreaker: String?, size: Int?) {
 
     //Сравниваем введенные строки
-    if (inputCoder.equals(inputCoderBreaker)) {
+    if (inputCoder.equals(inputCoderbreaker)) {
         println("Letters are equal. You win!")
         return
     }
 
     //Переменная, которая хранит символы, совпадающие по индексу
-    val saveIndexByLetters = saveIndexByLetters(inputCoder, inputCoderBreaker)
+    val matchingLetters = matchingLetters(inputCoder, inputCoderbreaker)
 
     //Переменная, которая хранит символы, не совпадающие по индексу
-    val saveLettersNotIndex = saveLettersNotIndex(inputCoder, inputCoderBreaker)
+    val notMatchingLetters = notMatchingLetters(inputCoder, inputCoderbreaker)
 
-    inputResult(inputCoder, saveIndexByLetters, saveLettersNotIndex, size)
+    inputResult(inputCoder, matchingLetters, notMatchingLetters, size)
 
 }
 
-fun saveIndexByLetters(inputCoder: String?, inputCoderBreaker: String?): Map<Int, Char> {
+fun matchingLetters(inputCoder: String?, inputCodebreaker: String?): Map<Int, Char> {
     //Переменная, которая хранит символы, совпадающие по индексу
-    val list = mutableMapOf<Int, Char>()
+    val matchingLetters = mutableMapOf<Int, Char>()
 
     for (indexCoderLetters in inputCoder?.indices!!) {
-        for (indexKit in inputCoderBreaker?.indices!!) {
+        for (indexKit in inputCodebreaker?.indices!!) {
             //Сравниваем индексы
             if (indexCoderLetters == indexKit) {
                 //если индексы равны, то сравниваем символы
-                if (inputCoder[indexCoderLetters] == inputCoderBreaker[indexKit]) {
+                if (inputCoder[indexCoderLetters] == inputCodebreaker[indexKit]) {
 
                     //если символы равны, то записываем их в переменную, которая хранит одинаковые символы по индексу
-                    list[indexKit] = inputCoderBreaker[indexKit]
+                    matchingLetters[indexKit] = inputCodebreaker[indexKit]
                 }
             }
         }
     }
-    return list
+    return matchingLetters
 }
 
-fun saveLettersNotIndex(inputCoder: String?, inputCoderBreaker: String?): MutableSet<Char> {
+fun notMatchingLetters(inputCoder: String?, inputCodebreaker: String?): MutableSet<Char> {
 
     //Переменная, которая хранит символы, не совпадающие по индексу
-    val list2 = mutableSetOf<Char>()
+    val notMatchingLetters = mutableSetOf<Char>()
 
     for (indexCoderLetters in inputCoder?.indices!!) {
-        for (indexKit in inputCoderBreaker?.indices!!) {
+        for (indexKit in inputCodebreaker?.indices!!) {
             //Сравниваем индексы
             if (indexCoderLetters != indexKit) {
                 //Если индексы не равны, то сравниваем символы
-                if (inputCoder[indexCoderLetters] == inputCoderBreaker[indexKit]) {
+                if (inputCoder[indexCoderLetters] == inputCodebreaker[indexKit]) {
                     //если символы равны, то записываем их в переменную, которая хранит одинаковые символы по индексу
-                    list2.add(inputCoderBreaker[indexKit])
+                    notMatchingLetters.add(inputCodebreaker[indexKit])
                 }
             }
         }
     }
-    return list2
+    return notMatchingLetters
 }
 
-fun inputResult(inputCoder: String?, saveIndexByLetters: Map<Int, Char>, saveLettersNotIndex: MutableSet<Char>, size: Int?) {
+fun inputResult(inputCoder: String?, matchingLetters: Map<Int, Char>, notMatchingLetters: MutableSet<Char>, size: Int?) {
 
-    if (saveIndexByLetters.isEmpty() && saveLettersNotIndex.size == 0) {
+    if (matchingLetters.isEmpty() && notMatchingLetters.size == 0) {
 
         println("Unfortunately, none of the letters match.")
 
-        compareTwoLetters(inputCoder, inputCoderBreaker(size), size)
+        compareTwoLetters(inputCoder, inputCodebreaker(size), size)
 
-    } else if (saveLettersNotIndex.size != 0 && saveIndexByLetters.isEmpty()) {
+    } else if (notMatchingLetters.size != 0 && matchingLetters.isEmpty()) {
 
-        iterate(saveLettersNotIndex)
+        iterate(notMatchingLetters)
 
-        compareTwoLetters(inputCoder, inputCoderBreaker(size), size)
+        compareTwoLetters(inputCoder, inputCodebreaker(size), size)
 
-    } else if (saveLettersNotIndex.size == 0) {
+    } else if (notMatchingLetters.size == 0) {
 
-        iterate(saveIndexByLetters)
+        iterate(matchingLetters)
 
-        compareTwoLetters(inputCoder, inputCoderBreaker(size), size)
+        compareTwoLetters(inputCoder, inputCodebreaker(size), size)
 
     } else {
 
-        iterate(saveLettersNotIndex)
+        iterate(notMatchingLetters)
 
-        iterate(saveIndexByLetters)
+        iterate(matchingLetters)
 
-        compareTwoLetters(inputCoder, inputCoderBreaker(size), size)
+        compareTwoLetters(inputCoder, inputCodebreaker(size), size)
     }
 }
 
-fun iterate(saveLettersNotIndex: MutableSet<Char>) {
+fun iterate(notMatchingLetters: MutableSet<Char>) {
 
     print("Letters that are present in the word that was entered by the coder : ")
 
-    for (element in saveLettersNotIndex) {
+    for (element in notMatchingLetters) {
         print("$element ")
     }
 
 }
 
-fun iterate(saveIndexByLetters: Map<Int, Char>) {
+fun iterate(matchingLetters: Map<Int, Char>) {
 
     println("\nLetters that match by index : ")
 
-    for ((key, value) in saveIndexByLetters) {
+    for ((key, value) in matchingLetters) {
         println("index is $key, letter is $value")
     }
 
